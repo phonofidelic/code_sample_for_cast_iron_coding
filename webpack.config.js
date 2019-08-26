b6b83a3f621997435d56a4ac90bccff276f6c83a
phonofidelic <chris.clemons85@gmail.com>
2019-08-25 23:56:53 -0700

    add background image to landing component

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,16 @@ const config = {
           },
           'css-loader'
         ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ]
       }
     ]
   },
