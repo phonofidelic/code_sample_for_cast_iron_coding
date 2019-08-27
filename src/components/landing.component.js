54696930b93164e6959667411343e26da81a5164
phonofidelic <chris.clemons85@gmail.com>
2019-08-26 20:56:29 -0700

    add background image layers to landing component

--- src/components/landing.component.js
+++ src/components/landing.component.js
@@ -1,7 +1,21 @@
 export default () => {
+  const landingContainer = document.createElement('div');
+  landingContainer.className = 'landing-container';
+
   const landing = document.createElement('div');
   landing.className = 'landing';
-  landing.innerHTML = `<p>landing</p>`;
 
-  return landing;
+  const hello = document.createElement('div');
+  hello.className = 'hello';
+  const cast = document.createElement('div');
+  cast.className = 'cast';
+  const iron = document.createElement('div');
+  iron.className = 'iron';
+
+  landingContainer.appendChild(landing);
+  landingContainer.appendChild(hello);
+  landingContainer.appendChild(cast);
+  landingContainer.appendChild(iron);
+
+  return landingContainer;
 };
