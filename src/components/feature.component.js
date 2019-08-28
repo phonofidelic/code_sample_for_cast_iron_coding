456fb7ea3e5a59bac400240829ce154b620eab28
Christopher Clemons <chris.clemons85@gmail.com>
2019-08-27 22:23:10 -0700

    add projects, adjust features styles

--- src/components/feature.component.js
+++ src/components/feature.component.js
@@ -1,14 +1,21 @@
 export default data => {
   const feature = document.createElement('div');
   feature.className = 'feature';
+  feature.style.backgroundImage = `url('${data.image}')`;
+  feature.addEventListener('click', e => console.log('click!', data));
 
   const featureTitle = document.createElement('h3');
   featureTitle.className = 'feature-title';
   featureTitle.innerText = data.title;
   feature.appendChild(featureTitle);
 
+  // const featureThumbnail = document.createElement('img');
+  // featureThumbnail.setAttribute('src', data.image);
+  // featureThumbnail.setAttribute('width', '100%');
+  // feature.appendChild(featureThumbnail);
+
   const featureCaption = document.createElement('p');
-  featureCaption.className = 'feature-caprion';
+  featureCaption.className = 'feature-caption';
   featureCaption.innerText = data.caption;
   feature.appendChild(featureCaption);
 
--- src/components/main.component.js
+++ src/components/main.component.js
@@ -1,25 +1,41 @@
 import Component from '../lib/Component';
 import section from './section.component';
 import featuresContainer from './featureContainer.component';
+import project1 from '../../assets/project_tapes.png';
+import project2 from '../../assets/project_JukeBox.png';
+import project3 from '../../assets/project_EDGalaxyMap.png';
 
 const features = [
   {
-    title: 'Feature #1',
-    caption: 'This is a short description',
+    title: 'Tapes',
+    caption:
+      'An intuitive interface for creating and cataloging audio recordings.',
     description: `This is a longer description, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-    image: 'http://lorempixel.com/200/200/'
+    image: project1
   },
   {
-    title: 'Feature #2',
-    caption: 'This is a short description',
+    title: 'Jukebox',
+    caption: 'A web-app for uploading and streaming audio files.',
     description: `This is a longer description, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-    image: 'http://lorempixel.com/200/200/'
+    image: project2
   },
   {
-    title: 'Feature #3',
+    title: 'EDGalaxy Map',
+    caption: 'A web app for viewing the Elite Dangerous Galaxy Map in VR.',
+    description: `This is a longer description, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+    image: project3
+  },
+  {
+    title: 'Feature #4',
     caption: 'This is a short description',
     description: `This is a longer description, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-    image: 'http://lorempixel.com/200/200/'
+    image: 'http://lorempixel.com/300/200/'
+  },
+  {
+    title: 'Feature #5',
+    caption: 'This is a short description',
+    description: `This is a longer description, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+    image: 'http://lorempixel.com/300/200/'
   }
 ];
 
