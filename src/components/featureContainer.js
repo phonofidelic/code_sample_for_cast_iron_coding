5be99a3acfbe4ee8899c3245e54f118fba5efc82
phonofidelic <chris.clemons85@gmail.com>
2019-08-29 13:04:50 -0700

    remove console logs

--- src/components/featureContainer.js
+++ src/components/featureContainer.js
@@ -4,9 +4,7 @@ export default features => {
   const featuresContainer = document.createElement('div');
   featuresContainer.className = 'features-container';
 
-  console.log('features:', features);
   features.forEach(featureData => {
-    console.log(featureData);
     featuresContainer.appendChild(feature(featureData));
   });
 
